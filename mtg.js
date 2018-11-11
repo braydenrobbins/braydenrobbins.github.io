7c26cdb8fa51f4760d0488ad7be9625528650069
Brayden Robbins <braydenrobbins98@gmail.com>
2018-11-11 16:31:01 -0600

    put the images into rows of 12

--- mtg.js
+++ mtg.js
@@ -1,8 +1,17 @@
 $(function() {
     $.get("https://api.magicthegathering.io/v1/cards?set=LEA|LEB|2ED|3ED&rarity=Rare", function(data){
         var cardList = $("#cardList");
-        cardList.html(data.cards.map(function(card) {
-            return "<li><img src='" + card.imageUrl + "'/></li>" 
-        }).join(""));
+        var cards = data.cards.map(function(card, index) {
+            var myHtml = ""
+            if (index === 0) {
+                myHtml += "<div class='row'>"
+            } else if (index % 12 === 0) {
+                 myHtml += "</div><div class='row'>"
+             }
+            myHtml += "<div class='col-md-1'><img src='" + card.imageUrl + "'/></div>" 
+            return myHtml
+        }).join("\n")
+        console.log(cards)
+        cardList.html(cards);
     })
 })
