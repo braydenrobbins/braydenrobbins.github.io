39a94bc5cce10132c77f729a339af3e8dc371eaf
Brayden Robbins <braydenrobbins98@gmail.com>
2018-11-24 03:01:09 -0600

    set up the basic functions for the new version of the project to display card images upon search

--- mtg.js
+++ mtg.js
@@ -1,17 +1,33 @@
 $(function() {
     $.get("https://api.magicthegathering.io/v1/cards?set=LEA|LEB|2ED|3ED&rarity=Rare", function(data){
         var cardList = $("#cardList");
-        var cards = data.cards.map(function(card, index) {
+        var cardName = ""
+        
+        function inputChange(event){
+            console.log(event.target.value)
+            cardName = event.target.value
+        }
+
+        function displayCard() {
             var myHtml = ""
-            if (index === 0) {
-                myHtml += "<div class='row'>"
-            } else if (index % 4 === 0) {
-                 myHtml += "</div><div class='row'>"
-             }
-            myHtml += "<div class='col-md-3'><img src='" + card.imageUrl + "'/></div>" 
-            return myHtml
-        }).join("\n")
-        console.log(cards)
-        cardList.html(cards);
+            var currentCard = data.cards.reduce(x => x.name === cardName)
+            myHtml += "<img src='" + currentCard.imageUrl + "'/></div>"
+            
+        }
+
+
+
+        // var cards = data.cards.map(function(card, index) {
+        //     var myHtml = ""
+        //     if (index === 0) {
+        //         myHtml += "<div class='row'>"
+        //     } else if (index % 4 === 0) {
+        //          myHtml += "</div><div class='row'>"
+        //      }
+        //     myHtml += "<div class='col-md-3'><img src='" + card.imageUrl + "'/></div>" 
+        //     return myHtml
+        // }).join("\n")
+        // console.log(cards)
+        // cardList.html(cards);
     })
 })
